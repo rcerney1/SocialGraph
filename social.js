994ba43031dfbd24e018ad16a4a9568f82d39f06
rcerney <ryanccerney@gmail.com>
2024-05-08 22:10:08 -0700

    socialNetwork, addUser done

--- social.js
+++ social.js
@@ -8,7 +8,18 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here 
+    // Your code here
+    // create an obj with an ID and name
+    let userID = this.currentID + 1;
+    let newUser = {'id' : userID, 'name': name} 
+    // id should be this.currentID + 1
+    
+    this.users[userID] = newUser;
+    // initialize a new set to track who the user follows --> this.follows[id] = new Set()
+    this.follows[userID] = new Set();
+    // returns unique ID
+    this.currentID++;
+    return userID;
   }
 
   getUser(userID) {
@@ -31,5 +42,12 @@ class SocialNetwork {
     // Your code here 
   }
 }
+
+socialNetwork = new SocialNetwork()
+//socialNetwork.addUser('John Doe')
+console.log(socialNetwork)
+
+console.log(Object.keys(socialNetwork.follows).length)
+
 
 module.exports = SocialNetwork;
